da439213bba2c1dc1202d570a000ed112ce5efc0
QiufanXu <519893120@qq.com>
2016-02-24 17:14:23 +0800

    Main Function

---
+++ src/Start.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author Qiufan(Andy) Xu 
+ * @date CreateTime: Feb 24, 2016 5:14:04 PM 
+ * @version 1.0 
+ * @param 
+ */
+
+public class Start {
+
+	public static void main(String[] args) {
+		/**
+		 * 
+		 * @param 
+		 * @return 
+		 */
+		// TODO Auto-generated method stub
+	}
+
+}
